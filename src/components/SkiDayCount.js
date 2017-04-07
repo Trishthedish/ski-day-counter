e5ffd4ed320b9ea368dfe19fa52860023f4504a7
Trish Griego <trish.griego@moz.com>
2017-04-07 12:37:47 -0700

    added minimum code to SkiDayCount.js

---
+++ src/components/SkiDayCount.js
@@ -0,0 +1,16 @@
+// SkiDayCount.js
+import React from 'react'
+import '..stylesheets.ui.scsss'
+
+const SkiDayCount = React.createClass({
+  render: function() {
+    return (
+      <div className="ski-day-count">
+
+
+      </div>
+    );
+  }
+    )
+  }
+})
