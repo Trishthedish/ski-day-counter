de943229ae34415ebd363ad3714737d47d9b775e
Trish Griego <trish.griego@moz.com>
2017-04-17 12:15:24 -0700

    Added a gitignore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import { render } from 'react-dom'
 import './stylesheets/ui.scss'
 import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
+import { HashRouter, Route, Link } from 'react-router-dom'
 
 window.React = React
 
-render (
-	<App />,
+render(
+	<HashRouter>
+		<div>
+			<Route path="/" component={App}/>
+			<Route path="*" component={Whoops404}/>
+		</div>
+	</HashRouter>,
 	document.getElementById('react-container')
 )
