3d9dfe3d5af4ac4250bb1b1f207112f88d6afa36
Trish Griego <trish.griego@moz.com>
2017-04-17 14:36:29 -0700

    Addded changed import statement to include 'Hashrouter', 'Switch', 'Route' 'link' from 'react-route-dom'. Diverted path from tutorial. In order to make routes work with the newest version of react-router.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import { render } from 'react-dom'
 import './stylesheets/ui.scss'
 import { App } from './components/App'
 import { Whoops404 } from './components/Whoops404'
-import { HashRouter, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 window.React = React
 
 render(
-	<HashRouter>
-		<div>
-			<Route path="/" component={App}/>
-			<Route path="*" component={Whoops404}/>
-		</div>
-	</HashRouter>,
-	document.getElementById('react-container')
+  <Router>
+    <Switch>
+      <Route exact={true} path="/" component={App} />
+      <Route path="*" component={Whoops404}/>
+    </Switch>
+  </Router>,
+  document.getElementById('react-container')
 )
