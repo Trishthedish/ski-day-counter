64219097ad278fbec0b77e61b4178d6fd44eab5b
Trish Griego <trish.griego@moz.com>
2017-04-17 15:59:58 -0700

    Rolled back some modules to earlier versions. Fixed importing of picture. Should be up to date of Chapter 6. 06_05

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,17 @@
-import { link } from 'react-router'
+import { Link } from 'react-router'
 import HomeIcon from 'react-icons/lib/fa/home'
 import AddDayIcon from 'react-icons/lib/fa/calendar-plus-o'
-import ListDaysIcons from 'react-icons/lib/fa/table'
-
+import ListDaysIcon from 'react-icons/lib/fa/table'
 
 export const Menu = () =>
-  <nav className="menu">
-    <Link to="/" activeClassName="selected">
-      <HomeIcon />
-    </Link>
-    <Link to="/add-day" activeClassName="selected">
-      <AddDayIcon />
-    </Link>
-    <Link to="list-days" activeClassName="selected">
-      <ListDaysIcons/>
-    </Link>
-  </nav>
+	<nav className="menu">
+		<Link to="/" activeClassName="selected">
+			<HomeIcon />
+		</Link>
+		<Link to="/add-day" activeClassName="selected">
+			<AddDayIcon />
+		</Link>
+		<Link to="/list-days" activeClassName="selected">
+			<ListDaysIcon />
+		</Link>
+	</nav>
